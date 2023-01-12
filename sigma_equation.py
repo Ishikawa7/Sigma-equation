65108752764bf38d9c520e5415d6d36cf4493571
Ishikawa7 <davidegrimaldi92@gmail.com>
2023-01-12 11:09:45 +0100

    first commit

---
+++ sigma_equation.py
@@ -0,0 +1,13 @@
+import numpy as np
+import math
+
+# input variables
+cecita = 0.5
+complessita = 0.5
+disistima_espressa = 0.5
+disistima_ricevuta = 0.5
+rifiuto_effettivo = 0.5
+
+tensione0 = cecita + complessita
+diff0 = abs(complessita - cecita)
+# continue
