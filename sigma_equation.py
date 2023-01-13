21b88734366424de6d32a50771ca0f2ffb74ad21
Ishikawa7 <davidegrimaldi92@gmail.com>
2023-01-13 18:25:01 +0100

    highlited differences beetween book and c code derived sigma

--- sigma_equation.py
+++ sigma_equation.py
@@ -16,24 +16,28 @@
 #	diff = compl - cieco;
 #	diff1 = cieco - compl;
 #	coef = 1;
+
 #	if (compl > cieco)
 #		coef=1-pow(1+pow((1-pow(diff,2))/diff,2),-1);
 #	if (compl < cieco)
 #		coef=1-pow(1+pow((1-pow(diff1,4))/pow(diff,2),2),-1);
+
 #	tensione0 = tensione0 * coef;
 #	diff = ripre - cieco;
 #	diff1 = cieco - ripre;
 #	coef = 1;
+
 #	if (ripre > cieco)
 #		coef=1-pow(1+pow((1-pow(diff,2))/sqrt(pow(diff,2)),2),-1);
 #	if (ripre < cieco)
 #		coef=1-pow(1+pow((1-pow(diff1,4))/sqrt(pow(diff,4)),2),-1);
+
 #	tensione1 = ripre * coef;
 #	tensione =(tensione0+pow(tensione1,2))-(tensione0*pow(tensione1,2));
 #	stima = pow(disis,2) * pow(ripre,2);
 #	tensione2 = stima +pow(rifef,2);
 #	lavoro= (tensione + tensione2)*100;
-#return(lavoro);
+#   return(lavoro);
 #}
 
 # import sqrt function
@@ -55,12 +59,13 @@ def sigma_equation_book(cecita, complessita, disistima_espressa, disistima_ricev
     tensione0 = cecita + complessita #cieco nel libro
     diff0 = abs(complessita - cecita)
     if complessita > cecita:
-        coef0 = 1 - (1 + ( (1-diff0**2) / diff0 )**2 )**(-1)
+        coef0 = 1 - (1 + ( (1 - diff0**2) / diff0 )**2 )**(-1)
     if complessita < cecita:
-        coef0 = 1 - (1 + ( (1-diff0**4) / (diff0)**2 )**2 )**(-1)
+        coef0 = 1 - (1 + ( (1 - diff0**4) / (diff0)**2 )**2 )**(-1)
+    
     tensione0 = tensione0 * coef0
-    diff1 = abs(disistima_espressa - tensione0)
 
+    diff1 = abs(disistima_espressa - tensione0)
     if disistima_espressa > tensione0:
         coef1 = 1 - (1 + ( (1-diff1**2) / diff1 )**2 )**(-1)
     if disistima_espressa < tensione0:
@@ -74,32 +79,29 @@ def sigma_equation_book(cecita, complessita, disistima_espressa, disistima_ricev
     return LAVORO
 
 # SIGMA EQUATION C CODE
-
-def sigma_equation_c_code_2(cecita, complessita, disistima_espressa, disistima_ricevuta, rifiuto_effettivo):
+# comments highligts the differences between the book and the c code
+def sigma_equation_c_code(cecita, complessita, disistima_espressa, disistima_ricevuta, rifiuto_effettivo):
     tensione0 = cecita + complessita
-    diff = complessita - cecita
+    diff = complessita - cecita #valore assoluto e una sola differenza
     diff1 = cecita - complessita
     coef = 1
     if complessita > cecita:
-        coef = 1 - (1 + (1 - (diff ** 2))/diff ** 2) ** -1
+        coef = 1 - (1 + (1 - (diff ** 2))/diff ** 2) ** -1 #quadrato su diff
     if complessita < cecita:
         coef = 1 - (1 + (1 - (diff1 ** 4))/(diff ** 2) ** 2) ** -1
     tensione0 = tensione0 * coef
 
-    diff = disistima_espressa - cecita
-    diff1 = cecita - disistima_espressa
+    diff = disistima_espressa - cecita #valore assoluto e una sola differenza
+    diff1 = cecita - disistima_espressa # disistima espressa - cecita in vece che - tensione0
     coef = 1
     if disistima_espressa > cecita:
-        coef = 1 - (1 + (1 - (diff ** 2))/sqrt(diff ** 2)) ** -1
+        coef = 1 - (1 + (1 - (diff ** 2))/sqrt(diff ** 2)) ** -1 #quadrato su diff e radice quadrata
     if disistima_espressa < cecita:
-        coef = 1 - (1 + (1 - (diff1 ** 4))/sqrt(diff ** 4)) ** -1
+        coef = 1 - (1 + (1 - (diff1 ** 4))/sqrt(diff ** 4)) ** -1 #elevamento alla quarta invece che quadrato e radice quadrata
     tensione1 = disistima_espressa * coef
 
     tensione = (tensione0 + tensione1 ** 2) - (tensione0 * tensione1 ** 2)
     stima = disistima_ricevuta ** 2 * disistima_espressa ** 2
     tensione2 = stima + rifiuto_effettivo ** 2
-    lavoro = (tensione + tensione2) * 100
-
-    return lavoro
-
-
+    LAVORO = (tensione + tensione2) * 100
+    return LAVORO
